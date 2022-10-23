81f8b2563c34169871a5f81a5a548eab4c0ddad0
Dylan Bienstock <dylanbien@gmail.com>
2022-10-23 18:08:27 -0400

    triggers updates

--- triggers.sql
+++ triggers.sql
@@ -1,19 +1,30 @@
 -- -----------------------------------------------------
+-- View IcodeNumReviewers 
+-- -----------------------------------------------------
+
+DROP VIEW IF EXISTS IcodeNumReviewers;
+
+CREATE VIEW IcodeNumReviewers AS
+    Select  ICode, count(*) as reviewerCount 
+	FROM ICode
+	JOIN ReviewerGroup on ICode.ICode = ReviewerGroup.ICode_ICode
+	JOIN Reviewer on Reviewer.Users_idReviewer = ReviewerGroup.Reviewer_Users_idReviewer
+	GROUP BY ICode;
+
+-- -----------------------------------------------------
 -- Trigger after_new_manuscript_submitted 
 -- -----------------------------------------------------
 
-DELIMITER $$
-
-DROP TRIGGER IF EXISTS after_new_manuscript_submitted;
-
-CREATE TRIGGER after_new_manuscript_submitted
-    AFTER INSERT ON Manuscript
+CREATE TRIGGER before_manuscript_submit
+    BEFORE INSERT ON Manuscript
     FOR EACH ROW 
-    BEGIN
+		IF (ICodeId NOT IN (select ICode from IcodeNumReviewers)) 
+			THEN
+             set @msg = concat('LAB2: No reviewers for manuscript id ', cast(idManuscript as char));
+			 signal sqlstate '45000' set message_text = @msg;
 
-    END$$
+    END IF$$
 DELIMITER ;
-
 
 -- -----------------------------------------------------
 -- Trigger after_author_deleted
@@ -43,17 +54,14 @@ DELIMITER ;
 -- Trigger after_manuscript_updated
 -- -----------------------------------------------------
 
+DROP TRIGGER IF EXISTS before_manuscript_updated;
 DELIMITER $$
-
-DROP TRIGGER IF EXISTS after_manuscript_updated;
-
-CREATE TRIGGER after_manuscript_updated
+CREATE TRIGGER before_manuscript_updated
   BEFORE UPDATE ON Manuscript
   FOR EACH ROW 
   BEGIN
-	  IF new.idManuscript = old.idManuscript && old.status = 'Accepted' THEN
-		  SET NEW.Status = 'TypeSetting';
-		END IF;
+	  IF NEW.ManStatus = 'Accepted' THEN
+	    SET NEW.ManStatus = 'TypeSetting';
+	  END IF;
 	END$$
 DELIMITER ;
-
