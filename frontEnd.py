dcfa1d2af41c5087c12257db8f682178cf8e1da0
Dylan Bienstock <dylanbien@gmail.com>
2022-10-26 10:21:51 -0400

    initial python commits

---
+++ frontEnd.py
@@ -0,0 +1,52 @@
+from mysql.connector import MySQLConnection, Error, errorcode, FieldType
+from dbconfig import read_db_config
+import getpass
+
+
+def frontend():
+
+
+    dbconfig = read_db_config()
+    if dbconfig['password'] == "":  
+        dbconfig['password'] = getpass.getpass("database password ? :")
+    
+    print(dbconfig)
+        
+    # Connect to the database
+    try:
+        print('Connecting to MySQL database...')
+        conn = MySQLConnection(**dbconfig)
+ 
+        if conn.is_connected():
+            print('connection established.')
+            mycursor = conn.cursor(buffered=True)
+        else:
+            print('connection failed.')
+
+    except mysql.connector.Error as err:
+        print('connection failed somehow')
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print("Unexpected error")
+            print(err)
+            sys.exit(1)
+
+
+    while True:
+        command = input("Welcome. What would you like to do: ").lower()
+        if command == 'done':
+            break
+
+
+
+    mycursor.close()
+    conn.cmd_reset_connection()
+    conn.close()
+    print("\nDONE")
+
+
+if __name__ == '__main__':
+    frontend()
